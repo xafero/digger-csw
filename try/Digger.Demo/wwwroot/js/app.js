069c9aa2303f960a4ab13c1cff9362dabdb6e894
xafero <xafero@users.noreply.github.com>
2023-09-02 02:55:14 +0200

    Adding keyboard

---
+++ try/Digger.Demo/wwwroot/js/app.js
@@ -0,0 +1,29 @@
+
+function serializeEvent(e) {
+    if (e) {
+        var o = {
+            altKey: e.altKey,
+            charCode: e.charCode,
+            code: e.code,
+            composed: e.composed,
+            ctrlKey: e.ctrlKey,
+            metaKey: e.metaKey,
+            key: e.key,
+            keyCode: e.keyCode,
+            repeat: e.repeat,
+            shiftKey: e.shiftKey,
+            timeStamp: e.timeStamp,
+            type: e.type,
+            which: e.which
+        };
+        return o;
+    }
+};
+
+document.addEventListener('keydown', function (e) {
+    DotNet.invokeMethodAsync('Digger.Demo', 'OnKeyDown', serializeEvent(e));
+});
+
+document.addEventListener('keyup', function (e) {
+    DotNet.invokeMethodAsync('Digger.Demo', 'OnKeyUp', serializeEvent(e));
+});
