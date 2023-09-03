b3f49e8477391311a4804f447519c405ac918d31
xafero <xafero@users.noreply.github.com>
2023-09-03 17:24:44 +0200

    Adding gamepad control per HTML api

--- try/Digger.Demo/wwwroot/js/app.js
+++ try/Digger.Demo/wwwroot/js/app.js
@@ -41,3 +41,74 @@ function doCanvasSetup() {
     window.addEventListener('DOMContentLoaded', resizeCanvas);
     resizeCanvas();
 }
+
+let listenToJoy = false;
+let lastJoyStates = [];
+
+function getJoyStates(gamepad) {
+    const array = [];
+    for (const button of gamepad.buttons) {
+        array.push(button.value);
+    }
+    return array;
+}
+
+function getArrayDiff(first, second) {
+    const array = [];
+    const max = Math.max(first.length, second.length);
+    for (let i = 0; i < max; i++) {
+        const firstI = first[i] ?? 0;
+        const secondI = second[i] ?? 0;
+        let res;
+        if (firstI === secondI) res = null;
+        else res = secondI - firstI;
+        array.push(res);
+    }
+    return array;
+}
+
+function sendKey(btnVal, btnKey) {
+    const evt = { Key: btnKey };
+    if (btnVal === 1)
+        DotNet.invokeMethodAsync('Digger.Demo', 'OnKeyDown', evt);
+    else
+        DotNet.invokeMethodAsync('Digger.Demo', 'OnKeyUp', evt);
+}
+
+function startJoyListen() {
+    const gamepads = navigator.getGamepads();
+    for (const gamepad of gamepads) {
+        if (gamepad) {
+            const myJoyStates = getJoyStates(gamepad);
+            const myJoyDiff = getArrayDiff(lastJoyStates, myJoyStates);
+            const btnFire = myJoyDiff[0];
+            if (btnFire) sendKey(btnFire, "Control");
+            const btnEsc = myJoyDiff[9];
+            if (btnEsc) sendKey(btnEsc, "Escape");
+            const btnMinus = myJoyDiff[4];
+            if (btnMinus) sendKey(btnMinus, "-");
+            const btnPlus = myJoyDiff[5];
+            if (btnPlus) sendKey(btnPlus, "+");
+            const btnLeft = myJoyDiff[14];
+            if (btnLeft) sendKey(btnLeft, "ArrowLeft");
+            const btnRight = myJoyDiff[15];
+            if (btnRight) sendKey(btnRight, "ArrowRight");
+            const btnUp = myJoyDiff[12];
+            if (btnUp) sendKey(btnUp, "ArrowUp");
+            const btnDown = myJoyDiff[13];
+            if (btnDown) sendKey(btnDown, "ArrowDown");
+            lastJoyStates = myJoyStates;
+        }
+    }
+    if (!listenToJoy) return;
+    requestAnimationFrame(startJoyListen);
+}
+
+window.addEventListener("gamepadconnected", (e) => {
+    listenToJoy = true;
+    requestAnimationFrame(startJoyListen);
+});
+
+window.addEventListener("gamepaddisconnected", (e) => {
+    listenToJoy = false;
+});
